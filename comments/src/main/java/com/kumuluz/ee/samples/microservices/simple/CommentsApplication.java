9f35cd56b7c43326e3b264ec09cd42b31e8ddee0
Rok Jaklič <rokj@rasca.net>
2018-01-08 16:20:08 +0100

    Update CommentsApplication.java

--- comments/src/main/java/com/kumuluz/ee/samples/microservices/simple/CommentsApplication.java
+++ comments/src/main/java/com/kumuluz/ee/samples/microservices/simple/CommentsApplication.java
@@ -1,8 +1,10 @@
 package com.kumuluz.ee.samples.microservices.simple;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService(value = "microservices-comments", ttl = 20, environment = "dev", version = "1.0.0")
 @ApplicationPath("/")
 public class CommentsApplication extends Application{
 }
